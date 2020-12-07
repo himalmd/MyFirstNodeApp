a7d961b9687d73172b686b15b0ae1056b096eaed
himalmd <dphmalith@gmail.com>
2020-12-07 13:00:54 +0530

    Update #2

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const EventEmitter = require('events');
-const emitter = new EventEmitter();
 
-emitter.on('messageLogged', function(){
-    console.log('Listener Called');
+const Logger = require('./logger');
+const logger = new Logger();
+
+logger.on('messageLogged', (arg) => {
+    console.log('Listener Called', arg);
 })
 
-emitter.emit('messageLogged');
+
+logger.log('message');
--- logger.js
+++ logger.js
@@ -1,7 +1,15 @@
+const EventEmitter = require('events');
+
 var url = "https://logger.io/log";
 
-function log(message){
-    console.log(message);
+class Logger extends EventEmitter{
+    log(message){
+        console.log(message);
+    
+        // Raise an event
+        this.emit('messageLogged', {id: 1, url: 'https://gg'});
+    }    
 }
 
-module.exports= log;
+
+module.exports = Logger;
